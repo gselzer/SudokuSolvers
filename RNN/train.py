91640920bcbb369640515f7ab139f5b9bbba7203
Gabriel Selzer <gjselzer@wisc.edu>
2021-11-28 11:38:36 -0600

    RNN: First cut
    The verdict: Right now it suks

--- RNN/train.py
+++ RNN/train.py
@@ -14,40 +14,47 @@ from tensorflow import keras
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-special_value = -10
 n2 = hp.puzzleSize
-LSTM_output_units = 128
+LSTM_output_units = 9
 batch_size = 100
 
-train_data = 'data/train3x3size100000.npz'
+train_data = 'data/debug_n100.npz'
 
 def load_data(filename):
     npzfile = np.load(filename)
     return npzfile['quizzes'], npzfile['solutions']
 
 def generate_model():
-    model = keras.Sequential()
-    # Not quite sure what this does, just following the in-class exercise
-    #model.add(keras.layers.Masking(mask_value=special_value, input_shape=(n2, n2, 1)))
+    # Input layer -> takes in a puzzle matrix
+    input_layer = keras.Input(shape=(n2, n2))
 
-    model.add(keras.layers.Conv2D(hp.num_filters, hp.filter_size, padding='same', activation='relu', input_shape=(n2, n2, 1)))
-    
-    for i in range(hp.num_blocks - 1):
-        model.add(keras.layers.Conv2D(hp.num_filters, hp.filter_size, padding='same', activation='relu'))
-    # Bidirectional LSTM
-    #model.add(keras.layers.Bidirectional(keras.layers.LSTM(LSTM_output_units)))
+    # Concatenate different processing layers
+    concat = []
+    # Processing layer 1 -> The provided clues
+    concat.append(keras.layers.Lambda(lambda x: x)(input_layer))
+
+    # Processing layer 2 -> LSTM on rows
+    concat.append(keras.layers.Bidirectional(keras.layers.LSTM(LSTM_output_units, return_sequences=True))(input_layer))
+
+    # Processing layer 2 -> LSTM on columns
+    transpose = keras.layers.Permute((2, 1))(input_layer)
+    concat.append(keras.layers.Bidirectional(keras.layers.LSTM(LSTM_output_units, return_sequences=True))(transpose))
+
+    # Concatenate each layer
+    concat_layer = keras.layers.Concatenate()(concat)
 
     # Dense Layer to convert to n^6 elements
-    model.add(keras.layers.Flatten())
-    model.add(keras.layers.Dense(n2 ** 3, activation='sigmoid'))
+    dense = keras.layers.Dense(n2 ** 2, activation='sigmoid')(concat_layer)
     # Reshape to n^2-by-n^2-by-n^2
-    model.add(keras.layers.Reshape((n2, n2, n2)))
+    outReshaped=keras.layers.Reshape((n2, n2, n2))(dense)
     # Softmax along last layer
-    model.add(keras.layers.Softmax(axis=-1))
+    out = keras.layers.Softmax(axis=-1)(outReshaped)
     
+    model = keras.models.Model(inputs=input_layer, outputs=out);
     model.compile(loss='categorical_crossentropy', optimizer='adam')
-    #print(model.summary())
+    print(model.summary())
     return model
+
 
 # Converts a 2D numpy solution to a 3D tensorflow tensor, where the third dimension is one-hot encoded.
 # Modified from https://stackoverflow.com/questions/58406795/how-to-convert-2d-numpy-array-to-one-hot-encoding
@@ -69,12 +76,12 @@ def solution_to_one_hot_tensor(solution):
 def main():
     model = generate_model()
     quizzes, solutions = load_data(train_data)
-    quizzes = quizzes.reshape(quizzes.shape[0], n2, n2, 1)
     solutions = np.array([solution_to_one_hot_tensor(x) for x in solutions])
     train_quizzes, test_quizzes, train_solutions, test_solutions = train_test_split(quizzes, solutions)
 
     model.fit(train_quizzes, train_solutions, batch_size=batch_size, epochs=hp.num_epochs, verbose=1)
 
+    print('Puzzle: \n', test_quizzes[0])
     pred_solutions = model.predict(test_quizzes)
     print('Expected: \n', np.argmax(test_solutions[0], axis=0) + 1)
     s = np.argmax(pred_solutions[0], axis=0) + 1
